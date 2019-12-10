5abc49e6270ac286c44d839bce4461d7d13eb543
Campo Elias Pinillos <campo.pinillos@gmail.com>
2019-12-10 18:34:53 -0500

    [Fix] Advanced task

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n >= 0:
-        string = str[:n] + str[n+1:]
-        return string
-    else:
-        return str
+        return ('{}{}'.format(str[:n], str[n+1:]))
+    return str
