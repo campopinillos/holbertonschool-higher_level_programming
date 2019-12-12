50e0bad5286cde9a1b68ed7f5af5837c01e46b3c
Campo Elias Pinillos <campo.pinillos@gmail.com>
2019-12-12 18:11:14 -0500

    [Fix] Mandatory taskscode should not be executed when imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-if len(sys.argv) - 1 > 0:
-    for i in range(1, len(sys.argv)):
-        sum += int(str(sys.argv[i]))
-print("{}".format(sum))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    if len(sys.argv) - 1 > 0:
+        for i in range(1, len(sys.argv)):
+            sum += int(str(sys.argv[i]))
+    print("{}".format(sum))
