30d29ea95c4dd31367d8bf0afb853e4c3129f050
Campo Elias Pinillos <campo.pinillos@gmail.com>
2019-12-12 18:47:37 -0500

    [Fix] Advanced task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,13 +3,12 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    n = len(sys.argv) - 1
     operator = {"+": add, "-": sub, "*": mul, "/": div}
 
-    if n != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in list(operator.keys()):
+    if sys.argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     print("{} {} {} = {}".format(int(sys.argv[1]),
