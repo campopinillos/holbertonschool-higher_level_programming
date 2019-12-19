9b72d695d5c63f7b888170e328e1ce6d99290b7e
Campo Elias Pinillos <campo.pinillos@gmail.com>
2019-12-19 17:36:58 -0500

    [Fix] reduce code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    g = my_list[:].sort()
-    filter(lambda x: g.remove(x) is None and g.count(x) == 0, my_list)
-    print(g)
-    return (sum(g))
+    return sum(list(set(my_list)))
