29713402185dd391e813e67755e690a937be9ad1
Campo Elias Pinillos <campo.pinillos@gmail.com>
2020-01-02 23:02:01 -0500

    [Feature] Advanced task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,94 @@
+#include "lists.h"
+listint_t *reverse_listint(listint_t **head);
+int compare(listint_t *list_1, listint_t *list_2);
+/**
+ * is_palindrome - Check is the listint is a Palindrome
+ * @head: type listint_s double pointer of node
+ *
+ * Return: 1 if is a pilndrome 0 if is not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *sl, *fas, *prev_sl;
+	listint_t *scn_half, *middle;
+	int palindrome = 1;
+
+	sl = fas = prev_sl = *head;
+	middle = NULL;
+	if (*head && (*head)->next)
+	{
+		while (fas && fas->next)
+		{
+			fas = fas->next->next;
+			prev_sl = sl;
+			sl = sl->next;
+		}
+
+		if (fas)
+		{
+			middle = sl;
+			sl = sl->next;
+		}
+
+		scn_half = sl;
+		prev_sl->next = NULL;
+		scn_half = reverse_listint(&scn_half);
+		palindrome = compare(*head, scn_half);
+
+		if (middle)
+		{
+			prev_sl->next = middle;
+			middle->next = scn_half;
+		}
+		else
+			prev_sl->next = scn_half;
+	}
+	return (palindrome);
+}
+
+/**
+ * reverse_listint - Reverses a listint_t linked list
+ * @head: Head
+ *
+ * Description: Reverses a listint_t linked list
+ * Return: A pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *head1, *tail;
+
+	if (!head && !*head)
+		return (NULL);
+	head1 = *head;
+	*head = NULL;
+	while (head1)
+	{
+		tail = head1;
+		head1 = head1->next;
+		tail->next = *head;
+		*head = tail;
+	}
+	return (*head);
+}
+
+/**
+ * compare - Reverses a listint_t linked list
+ * @list_1: Linked list 1
+ * @list_2: Linked list 2
+ *
+ * Description: Reverses a listint_t linked list
+ * Return: 1 if is a pilndrome 0 if is not
+ */
+int compare(listint_t *list_1, listint_t *list_2)
+{
+	while (list_1 != NULL && list_2 != NULL)
+	{
+		if (list_1->n == list_2->n)
+			list_1 = list_1->next, list_2 = list_2->next;
+		else
+			return (0);
+	}
+	if (!list_1 && !list_2)
+		return (1);
+	return (0);
+}
