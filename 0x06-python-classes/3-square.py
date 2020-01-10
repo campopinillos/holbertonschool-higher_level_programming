0364c8b2a8de4382483f6413719166532a46c773
campopinillos <1207@holbertonschool.com>
2020-01-09 20:13:41 -0500

    [Feature] Mandatory task 3 file upload

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """This is a class Square defined by size."""
+    def __init__(self, size=0):
+        """The __init__ method of the square class
+        Args:
+            size: Is the type int private attribute
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """The area public instance method of the square class"""
+        return (self.__size * self.__size)
