1381881ec256a34982aaa3c0d28769388c8335c4
campopinillos <1207@holbertonschool.com>
2020-01-13 22:33:48 -0500

    [Fix] Pycodestyle and cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,6 +17,12 @@ class TestMaxInteger(unittest.TestCase):
         f_l = [1.11, 2.22, 3.33, 4.44]
         self.assertEqual(max_integer(f_l), 4.44)
 
+    def test_num_string(self):
+        self.assertEqual(max_integer("1234567890"), "9")
+
+    def test_string(self):
+        self.assertEqual(max_integer("abcdefg"), "g")
+
 
 if __name__ == '__main__':
     unittest.main()
