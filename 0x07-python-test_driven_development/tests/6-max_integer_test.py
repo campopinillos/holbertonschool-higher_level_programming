562f4f96af82df754088aa613a282b3a0c48e1ee
campopinillos <1207@holbertonschool.com>
2020-01-13 22:27:23 -0500

    [Fix] Pycodestyle

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,20 +4,19 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """
     This is the class unittest for the max integer function
     """
-
     def test_sorted_list(self):
+        sorted_list = [1, 2, 3, 4]
+        self.assertEqual(max_integer(sorted_list), 4)
 
-       sorted_list = [1, 2, 3, 4]
-       self.assertEqual(max_integer(sorted_list), 4)
-    
     def test_float_list(self):
+        f_l = [1.11, 2.22, 3.33, 4.44]
+        self.assertEqual(max_integer(f_l), 4.44)
 
-        l = [1.11, 2.22, 3.33, 4.44]
-        self.assertEqual(max_integer(l), 4.44)
 
 if __name__ == '__main__':
     unittest.main()
