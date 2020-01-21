cdac67e4c34cebda39c821fdd379d119b24be155
campopinillos <1207@holbertonschool.com>
2020-01-21 18:39:56 -0500

    [Feature] Mandatory task file upload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Student Class
+
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation, constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Public method Retrieves a dictionary representation of a
+        Student instance
+        """
+        return self.__dict__
