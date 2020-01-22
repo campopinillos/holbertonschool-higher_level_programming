a0fdaa3961ed5fcce63d5a8ea538b37753963084
campopinillos <1207@holbertonschool.com>
2020-01-22 17:38:05 -0500

    [Feature] Mandatory task file upload

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    Pascal triangle
+    """
+    pascal_list = [[1]]
+    for i in range(1, n):
+        row = list(range(i + 1))
+        for j in range(i + 1):
+            if (j == 0 or j == i):
+                row[j] = 1
+            else:
+                row[j] = pascal_list[i - 1][j - 1] + pascal_list[i - 1][j]
+        pascal_list.append(row)
+    return pascal_list
