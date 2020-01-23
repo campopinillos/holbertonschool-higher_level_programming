62ab3708a38d9c119a0f4c6216cf8dbf0f7e6d30
campopinillos <1207@holbertonschool.com>
2020-01-22 20:30:00 -0500

    [Fix] super position

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,10 +11,10 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Instantiation, constructor"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """Area method"""
-        return (self.__size * self.__size)
+        return super().area()
