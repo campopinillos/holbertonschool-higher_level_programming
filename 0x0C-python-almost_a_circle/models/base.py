c7a9ef44608134c5fad8155bd5876a2fb4ccad7a
campopinillos <1207@holbertonschool.com>
2020-01-25 18:00:10 -0500

    [Update] Functions

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Base Model
 
 """
 import json
+import os
 
 
 class Base:
@@ -29,8 +30,8 @@ class Base:
         list_str = []
         if list_objs is not None:
             list_str = [l.to_dictionary() for l in list_objs]
-        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as new_file:
-            new_file.write(cls.to_json_string(list_str))
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_str))
 
     @staticmethod
     def from_json_string(json_string):
@@ -41,3 +42,19 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        ins = None
+        if cls.__name__ == "Rectangle":
+            ins = cls(1, 1)
+        elif cls.__name__ == "Square":
+            ins = cls(1)
+        ins.update(**dictionary)
+        return ins
+
+    @classmethod
+    def load_from_file(cls):
+        file = "{}.json".format(cls.__name__)
+        l_d = []
+        if os.path.exists(file):
+            with open(file, 'r') as f:
+                l_d = [cls.create(**d) for d in cls.from_json_string(f.read())]
+        return l_d
