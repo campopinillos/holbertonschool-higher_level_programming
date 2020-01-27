1ddcb537619d40fa25b8e3afafcc978bc9c40d1f
campopinillos <1207@holbertonschool.com>
2020-01-27 13:40:57 -0500

    [Fix] Docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,28 +17,34 @@ class Rectangle(Base):
         self.y = y
 
     def __str__(self):
+        """Print __str__"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                                                        self.__y, self.__width,
                                                        self.__height)
 
     @property
     def width(self):
+        """Width getter"""
         return self.__width
 
     @property
     def height(self):
+        """Height getter"""
         return self.__height
 
     @property
     def x(self):
+        """x getter"""
         return self.__x
 
     @property
     def y(self):
+        """y getter"""
         return self.__y
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -48,6 +54,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -57,6 +64,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        """x setter"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -66,6 +74,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        """y setter"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -74,9 +83,11 @@ class Rectangle(Base):
             self.__y = value
 
     def area(self):
+        """area public method"""
         return (self.__width * self.__height)
 
     def display(self):
+        """display public method"""
         if self.__width == 0 or self.__height == 0:
             print('')
         else:
@@ -90,6 +101,7 @@ class Rectangle(Base):
                 print('')
 
     def update(self, *args, **kwargs):
+        """update public method"""
         if args and len(args) != 0:
             i = 0
             att_list = ['id', 'width', 'height', 'x', 'y']
@@ -101,6 +113,7 @@ class Rectangle(Base):
                     setattr(self, k, v)
 
     def to_dictionary(self):
+        """to dictionary public method"""
         dic = {}
         i = 0
         att_list = ['id', 'width', 'height', 'x', 'y']
