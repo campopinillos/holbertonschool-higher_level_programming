edb93cfd46b26eacbd7f586ce27904b49abea1a4
campopinillos <1207@holbertonschool.com>
2020-05-05 17:56:18 -0500

    [Fix] Conditional input

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-if (process.argv.lenght === 2) {
+const myArgs = process.argv;
+if (myArgs.lenght === 2) {
   console.log('No argument');
-} else if (process.argv.lenght === 3) {
+} else if (myArgs.lenght === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
