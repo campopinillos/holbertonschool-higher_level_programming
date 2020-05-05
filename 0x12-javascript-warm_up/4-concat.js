6c02cdb265f8ad1eb948a1c0979d53a31b80668a
campopinillos <1207@holbertonschool.com>
2020-05-05 18:42:00 -0500

    [Fix] Undefined

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const varOne = process.argv[2];
+const varOne = process.argv[2] ? process.argv[2] : 'undefined';
 const varTwo = process.argv[3];
 console.log(varOne.concat(' is ', varTwo));
