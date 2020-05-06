c1ec457bc39249424284918b31da283b648e7868
campopinillos <1207@holbertonschool.com>
2020-05-06 01:02:00 -0500

    [Fix] Style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const arrayIni = process.argv;
 if (arrayIni.length <= 3) {
   console.log(0);
 } else {
-  arrayIni.splice(0, 2)
+  arrayIni.splice(0, 2);
   arrayIni.sort(function (a, b) { return b - a; });
   console.log(Number(arrayIni[1]));
 }
