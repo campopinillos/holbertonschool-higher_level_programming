c7f4bc3ab4530cdcdeaffcaa46ea58fc1404e782
campopinillos <1207@holbertonschool.com>
2020-05-05 23:27:49 -0500

    [Fix] NaN condition

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const times = process.argv[2];
-if (times > 0) {
+const times = parseInt(process.argv[2]);
+if (isNaN(times)) {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < times; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
