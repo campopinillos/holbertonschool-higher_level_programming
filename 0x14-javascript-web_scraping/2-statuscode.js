1518031534502644365b609ddb84dc6e06ecaa33
campopinillos <1207@holbertonschool.com>
2020-05-12 18:56:21 -0500

    [Fix] Code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,5 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (error, response) => {
-  console.log(`code: ${error.statusCode}` ||
-              `code: ${response.statusCode}`);
+  console.log(error || `code: ${response.statusCode}`);
 });
