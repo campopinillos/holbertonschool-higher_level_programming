3fe3a43281785d82fe954a2fb926aa6dc5052249
campopinillos <1207@holbertonschool.com>
2020-05-11 20:53:05 -0500

    [Fix] Code

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const firstList = require('./100-data').list;
-const secondList = firstList.map((item) => item * firstList.indexOf(item));
-
 console.log(firstList);
-console.log(secondList);
+console.log(firstList.map((item) => item * firstList.indexOf(item)));
