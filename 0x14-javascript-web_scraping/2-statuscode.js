2b35a7732077906a6ab50ad061efe12e2140690b
campopinillos <1207@holbertonschool.com>
2020-05-12 18:51:20 -0500

    [Feature] Mandatory task

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response) => {
+  console.log(`code: ${error.statusCode}` ||
+              `code: ${response.statusCode}`);
+});
