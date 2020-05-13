814be52f1b3d77522a33215c45538b3aa0023557
campopinillos <1207@holbertonschool.com>
2020-05-12 20:47:59 -0500

    [Fix] Code output

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `http://swapi.co/api/films/${process.argv[2]}/`;
-request(url, (error, httpResponse, body) => {
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, (error, response, body) => {
   console.log(JSON.parse(body).title || error);
 });
