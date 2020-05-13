9eee8ea391ea7a7be83ef72c40eef961f046803d
campopinillos <1207@holbertonschool.com>
2020-05-12 22:40:35 -0500

    [Fix] Code output and variables 1

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,11 +2,4 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-
-request(url, (error, response, body) => {
-  if (response) {
-    fs.writeFileSync(process.argv[3], body, 'utf-8');
-  } else {
-    console.log(error);
-  }
-});
+request(url).pipe(fs.WriteStream(process.argv[3], 'utf-8'));
