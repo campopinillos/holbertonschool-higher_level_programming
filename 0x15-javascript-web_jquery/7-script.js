bfb03c2b83409b786129f3ceef28d4fa9e685607
Campo Elias Pinillos <1207@holbertonschool.com>
2020-05-19 12:41:45 -0500

    [Fix] Code

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (responseText, statusText) => {
-  if (statusText === 'success') {
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (responseText, textStatus) => {
+  if (textStatus) {
     $('div#character').text(responseText.name);
   }
 });
