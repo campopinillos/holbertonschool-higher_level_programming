0cb474f82994e2b42c2e709ee546ea1257c59948
Campo Elias Pinillos <1207@holbertonschool.com>
2020-05-19 17:10:18 -0500

    [Fix] Document Object

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,4 @@
-$(function () {
+$('document').ready(function () {
   $.get('https://fourtonfish.com/hellosalut/?lang=fr', (responseText, textStatus) => {
     if (textStatus === 'success') {
       $('DIV#hello').text(responseText.hello);
